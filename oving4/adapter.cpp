07ce9c76c0d472fbe23bd18dd6aa00c099dcd34a
ingebart <ingenbrigt_bartholsen@hotmail.com>
2014-03-13 11:37:56 +0100

    til erlend som har kontorvakt

--- oving4/adapter.cpp
+++ oving4/adapter.cpp
@@ -1,7 +1,7 @@
 #include "adapter.h"
 
-#define freeloc 0x002C
-#define maxMemAdr 0x102C
+#define FREELOC 0x002C
+#define MAX_MEM_ADR 0x102C
 
 Adapter::Adapter(sc_module_name name, int addr) : sc_module(name){
 	address = addr;
@@ -20,10 +20,17 @@ void Adapter::pushed(int id){
 void Adapter::send(int id, int status){
 	int packet[3] = {3, id, status};
 	int nextFreeAddress;
-	adapterToBusPort->burst_read(id, &nextFreeAddress, freeloc, 1, false);
-
-	adapterToBusPort->burst_write(id, packet, nextFreeAddress, 3, false);
-
-	nextFreeAddress += 3 * 4; //incrementing 
- 
+	adapterToBusPort->burst_read(id, &nextFreeAddress, FREELOC, 1, true);
+	if (nextFreeAddress <= FREELOC)
+		nextFreeAddress = FREELOC +  4; //First possible address
+	else if (nextFreeAddress > (MAX_MEM_ADR - 12))
+		nextFreeAddress = FREELOC +  4; // Reseting the round-buffer register.
+	adapterToBusPort->burst_write(id, packet, nextFreeAddress, 3, true);
+	// Adding the nextFreeAddress and the adapter number into a 4 byte address.
+	// And sending it to the Control-register with address 0x0000;
+	packet[0] = (nextFreeAddress << 16)|(1<<id);
+	nextFreeAddress += 12;
+	adapterToBusPort->burst_write(id, &nextFreeAddress, FREELOC, 1, true); //updating the next address in FREELOC
+	adapterToBusPort -> burst_write(id, packet, 0x0000, 1, false); //sending to the Control-register.
 }
+	
--- oving4/adapter.h
+++ oving4/adapter.h
@@ -5,20 +5,17 @@
 # define   	ADAPTER_H
 
 #include <systemc.h>
-#include "if.h"
+//#include "if.h"
 #include "button.h"
 #include "simple_bus_slave_if.h"
 #include "simple_bus_blocking_if.h"
 
 
-const sc_time long_time (100, SC_MS);
-const sc_time short_time (30, SC_MS);
-
 //  Our beloved button class (changes are done)
 class Adapter : public sc_module, public simple_bus_slave_if, public bus_push_if{
 public:
   	Adapter (sc_module_name name, int _id);
-	sc_port<button_if> adapterToButtonPort;
+	sc_port<Button_if> adapterToButtonPort;
 	sc_port<simple_bus_blocking_if> adapterToBusPort;
 	void pushed(int);
 	int id;
