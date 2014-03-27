adc7de4850bdaf4387834a1a4da440f76cd1407b
Erlend <e.strandvik@gmail.com>
2014-03-27 09:59:34 +0100

    nytt

--- oving5/statemachine.cpp
+++ oving5/statemachine.cpp
@@ -1,2 +1,34 @@
 #include "statemachine.h"
 
+
+void FSM::fsm_functionality() {
+	Switch(CurrentState){
+		case S0:
+		if(pos_edge(tick)){
+			if(A >= limit){
+				NextState = S1;
+			}
+		}
+		break;
+
+		case S1:
+		if(pos_edge(tick)){
+			if(A < limit){
+				NextState = S0;
+			}else{
+				NextState = S2;
+			}
+		}
+		break;
+
+		case S2:
+		if(pos_edge(tick)){
+			if(A >= limit){
+				X->true;
+			}
+		}
+	}
+
+
+
+}
